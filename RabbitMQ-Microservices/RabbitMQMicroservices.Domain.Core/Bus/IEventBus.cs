a7a0fb826bdcf28f7187dc19bde42786b4091ea6
Uğur <ugurkk2599@gmail.com>
2022-05-01 23:17:37 +0300

    Interfaces for domain.core

---
+++ RabbitMQ-Microservices/RabbitMQMicroservices.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQMicroservices.Domain.Core.Commands;
+using RabbitMQMicroservices.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQMicroservices.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @bool) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
